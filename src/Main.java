1ec78d97e1c79ca8dde7c30a86bfeddc798bec86
Elzhan <124083366+elzhanxx@users.noreply.github.com>
2024-11-12 01:59:21 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+
+        while (true){
+            printMenu();
+            int userCommand = scanner.nextInt();
+            switch (userCommand){
+                case 1 -> ;
+                case 2 -> ;
+                case 3 -> ;
+                case 4 -> scanner.close();return;
+                default -> System.out.println("Такой команды нет");;
+            }
+        }
+    }
+
+    static void printMenu(){
+        System.out.println("""
+                    1) Ввести количество шагов за определённый день
+                    2) Изменить цель по количеству шагов в день
+                    3) Напечатать статистику за определённый месяц
+                    4) Выйти из приложения
+                    """);
     }
 }
